6df5bbb8c6faa038270a81e1e9ecdb938d30d430
Suryansh Gupta <54904738+Suryansh-2301@users.noreply.github.com>
2024-02-12 22:22:54 +0530

    uploaded fontawesome zip file by creating a static resource in salesforce. Also importing the same in JS file and loading it inside the renderedCallback() lifecycle hook

--- MemoryGame/force-app/main/default/lwc/memoryGameLWC/memoryGameLWC.js
+++ MemoryGame/force-app/main/default/lwc/memoryGameLWC/memoryGameLWC.js
@@ -1,3 +1,24 @@
 import { LightningElement } from 'lwc';
+import { loadStyle } from 'lightning/platformRersourceLoader';
+import fontawesome from '@salesforce/resourceUrl/fontawesome';
 
-export default class MemoryGameLWC extends LightningElement {}
+export default class MemoryGameLWC extends LightningElement {
+
+    // as renderedcallback is called everytime component is
+    //  refreshed so to stop loading the fontawesome we have created a checkbox
+    isLoaded = false;
+
+    renderedCallback() {
+        if (this.isLoaded) {
+            return;
+        } else {
+            loadStyle(this, fontawesome + '/fontawesome/css/font-awesome.min.css').then(() => {
+                console.log("loaded successfully");
+            }).catch(error => {
+                console.error(error);
+            })
+            this.isLoaded = true;
+        }
+
+    }
+}
